40a635509db3c3cc39afb969ec7c08cad0f05e8a
SaVyrin <sarajkinaa@gmail.com>
2022-08-14 17:20:54 +0300

    Add saving alarm in edit fragment

--- app/src/main/java/com/hfad/alarmclock/ui/fragments/editalarm/EditAlarmFragment.kt
+++ app/src/main/java/com/hfad/alarmclock/ui/fragments/editalarm/EditAlarmFragment.kt
@@ -5,16 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.hfad.alarmclock.R
+import com.hfad.alarmclock.data.database.Alarm
+import com.hfad.alarmclock.databinding.FragmentEditAlarmBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class EditAlarmFragment : Fragment() {
 
+    private val viewModel: EditAlarmViewModel by viewModels()
+
+    private var _binding: FragmentEditAlarmBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_edit_alarm, container, false)
+    ): View {
+        _binding = FragmentEditAlarmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setSaveButtonClickListener()
+        setNotSaveButtonClickListener()
+    }
+
+    private fun setSaveButtonClickListener() {
+        binding.saveBtn.setOnClickListener {
+            viewModel.saveAlarm(Alarm())
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun setNotSaveButtonClickListener() {
+        binding.notSaveBtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
