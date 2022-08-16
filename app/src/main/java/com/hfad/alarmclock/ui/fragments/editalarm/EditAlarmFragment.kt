a158da6b48656f32e8ab0c984aa5dd69d6ef1e09
SaVyrin <sarajkinaa@gmail.com>
2022-08-16 18:34:14 +0300

    Add correct navigation to edit alarms screen

--- app/src/main/java/com/hfad/alarmclock/ui/fragments/editalarm/EditAlarmFragment.kt
+++ app/src/main/java/com/hfad/alarmclock/ui/fragments/editalarm/EditAlarmFragment.kt
@@ -8,13 +8,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.hfad.alarmclock.data.database.Alarm
+import com.hfad.alarmclock.data.database.AlarmDao
 import com.hfad.alarmclock.databinding.FragmentEditAlarmBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class EditAlarmFragment : Fragment() {
 
-    private val viewModel: EditAlarmViewModel by viewModels()
+    @Inject
+    lateinit var alarmDao: AlarmDao
+    private lateinit var viewModelFactory: EditAlarmViewModelFactory
+    private val viewModel: EditAlarmViewModel by viewModels { viewModelFactory }
 
     private var _binding: FragmentEditAlarmBinding? = null
     private val binding get() = _binding!!
@@ -24,13 +29,33 @@ class EditAlarmFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentEditAlarmBinding.inflate(inflater, container, false)
+        setViewModelFactory()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeAlarm()
         setSaveButtonClickListener()
         setNotSaveButtonClickListener()
+    }
+
+    private fun setViewModelFactory() {
+        val alarmId = EditAlarmFragmentArgs.fromBundle(requireArguments()).alarmId
+        viewModelFactory = EditAlarmViewModelFactory(alarmId, alarmDao)
+    }
+
+    private fun observeAlarm() {
+        viewModel.alarm.observe(viewLifecycleOwner) { alarm ->
+            alarm?.also {
+                val alarmHoursAndMinutes = alarm.time.split(":")
+                val alarmHours = alarmHoursAndMinutes[0]
+                val alarmMinutes = alarmHoursAndMinutes[1]
+                binding.alarmHoursEdt.setText(alarmHours)
+                binding.alarmMinutesEdt.setText(alarmMinutes)
+                binding.alarmDescriptionEdt.setText(alarm.title)
+            }
+        }
     }
 
     private fun setSaveButtonClickListener() {
--- app/src/main/java/com/hfad/alarmclock/ui/fragments/editalarm/EditAlarmViewModel.kt
+++ app/src/main/java/com/hfad/alarmclock/ui/fragments/editalarm/EditAlarmViewModel.kt
@@ -1,17 +1,22 @@
 package com.hfad.alarmclock.ui.fragments.editalarm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hfad.alarmclock.data.database.Alarm
 import com.hfad.alarmclock.data.database.AlarmDao
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-@HiltViewModel
-class EditAlarmViewModel @Inject constructor(
+class EditAlarmViewModel(
+    alarmId: Long,
     private val alarmDao: AlarmDao
 ) : ViewModel() {
+
+    val alarm: LiveData<Alarm> = when (alarmId >= 0) {
+        true -> alarmDao.get(alarmId)
+        else -> MutableLiveData(Alarm())
+    }
 
     fun saveAlarm(alarm: Alarm) {
         viewModelScope.launch {
